0d716b9d159ac2558da5be050db53bc28add344a
hollisma <hollism@princeton.edu>
2020-11-28 18:14:26 -0500

    get and display cropped hand for each image

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const cameraSensor = document.querySelector('#camera-sensor')
+const cameraSensor = document.querySelector('#camera-sensor'),
+      saveCanvas = document.querySelector('#save-canvas')
 cameraSensor.width = 1280
 cameraSensor.height = 960
 ctx = cameraSensor.getContext('2d')
+saveCtx = saveCanvas.getContext('2d')
 
 gLow = 1, gHigh = 10, nLow = 1, nHigh = 10
 modelParams = {
-  detectionConfidence: 0,
+  detectionConfidence: 0.1,
   iouThreshold: 0,
   scoreThreshold: 0
 }
@@ -21,15 +23,23 @@ handpose.load(modelParams).then(model => {
     ctx.drawImage(img, 0, 0)
     
     model.estimateHands(cameraSensor).then(predictions => {
-      console.log(predictions)
-      // TODO crop image and save it somewhere
+      console.log(`detected ${predictions.length} hands for ${g}-${n}`)
+      
+      // Crop image to saveCanvas
+      if (predictions.length > 0) {
+        bb = predictions[0].boundingBox
+        tl = bb.topLeft, br = bb.bottomRight
+        newWidth = br[0] - tl[0], newHeight = br[1] - tl[1]
+        saveCanvas.width = newWidth
+        saveCanvas.height = newHeight
+        saveCtx.drawImage(img, tl[0], tl[1], newWidth, newHeight, 0, 0, newWidth, newHeight)
+      }
+      
+      // Get next image
+      g = n == nHigh ? g + 1 : g
+      n = n == nHigh ? nLow : n + 1
+      if (g < gHigh || n < nHigh) img.src = getFileName(g, n) 
     })
-    
-    g = n == nHigh ? g + 1 : g
-    n = n == nHigh ? nLow : n + 1
-    console.log(`new g and n: ${g}-${n}`)
-
-    if (g < gHigh || n < nHigh) img.src = getFileName(g, n)  // 6,1,9,4
   }
 
   
