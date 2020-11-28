b6dff92d426001fd07f9c5535291e115fd12cb4c
hollisma <hollism@princeton.edu>
2020-11-28 17:28:38 -0500

    detecting bounding box for a single image (need to serve images on server tho)

--- script.js
+++ script.js
@@ -1,42 +1,21 @@
-// const handpose = require('@tensorflow-models/handpose')
-// const tf = require('@tensorflow/tfjs')
-// const fs = require('fs')
-// require('@tensorflow/tfjs-backend-wasm')
-// // require('@tensorflow/tfjs-backend-webgl'); 
-
-// tf.setBackend('wasm').then(() => main())
-
-// let main = () => {
-//   handpose.load().then(model => {
-//     let num = 1
-//     fileName = `kinect_leap_dataset/acquisitions/P1/G1/${num}_rgb.png`
-//     fs.readFile(fileName, (err, data) => {
-//       if (err) console.log(err)
-//       console.log(`data is type ${data.length}`)
-//       model.estimateHands(data).then(predictions => {
-//         console.log(predictions)
-//       })
-//     })    
-//   })
-// }
-
-
-const thing = document.querySelector('#the-thing'),
-      cameraSensor = document.querySelector('#camera-sensor')
+const cameraSensor = document.querySelector('#camera-sensor')
+cameraSensor.width = 1280
+cameraSensor.height = 960
+ctx = cameraSensor.getContext('2d')
 
 handpose.load().then(model => {
-  let g = 5, 
-      num = 2
-  fileName = `kinect_leap_dataset/acquisitions/P1/G${g}/${num}_rgb.png`
+  g = 7, num = 2
+  fileName = `http://127.0.0.1:8080/kinect_leap_dataset/acquisitions/P1/G${g}/${num}_rgb.png`
   
-  thing.src = fileName
-  // thing.width = 1280
-  // thing.height = 960
-  cameraSensor.width = 1280
-  cameraSensor.height = 960
-  cameraSensor.getContext('2d').drawImage(thing, 0, 0)
-
-  model.estimateHands(cameraSensor).then(predictions => {
-    console.log(predictions)
-  })
+  img = new Image
+  img.crossOrigin = 'anonymous'
+  img.src = fileName
+  img.onload = () => {
+    ctx.drawImage(img, 0, 0)
+    
+    model.estimateHands(cameraSensor).then(predictions => {
+      console.log(predictions)
+    })
+  }
+  
 })
