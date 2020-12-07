939dd0909828031e40aa7a91d24e700ec231331e
Jessica Pan <jzpan17@gmail.com>
2020-12-06 23:05:58 -0500

    one last commit before I jump onto the yolo wagon

--- run_classifier.py
+++ run_classifier.py
@@ -7,6 +7,7 @@ def main():
     new_sample = json.loads(sys.argv[1])
     new_sample = np.array(new_sample).T
     new_sample = np.reshape(new_sample, (3, 21, 1, 1))
+    
     model = np.load('model.npz', allow_pickle=True)
     model = dict(model)
     output, _ = inference(model, new_sample)
