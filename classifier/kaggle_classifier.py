5e2322c21477da456ee24ac13b666f50540221b7
William Li <willpower264@gmail.com>
2020-12-08 13:59:18 -0800

    1 conv 4 linear - 0.94096 accuracy

--- classifier/kaggle_classifier.py
+++ classifier/kaggle_classifier.py
@@ -10,38 +10,30 @@ import time
 
 def main():
     train_data, train_label, test_data, test_label = load_data()
-    # layers = [init_layers('nxm_conv', {'filter_height': 1,
-    #                                    'filter_width': 3,
-    #                                    'filter_depth': 1,
-    #                                    'num_filters': 5}),
-    #           init_layers('relu', {}),
-    #           init_layers('flatten', {}),
-    #           init_layers('linear', {'num_in': 105,
-    #                                  'num_out': 6}),            
-    #           init_layers('softmax', {})]        
-    layers =  [
+    layers = [init_layers('nxm_conv', {'filter_height': 1,
+                                       'filter_width': 3,
+                                       'filter_depth': 1,
+                                       'num_filters': 5}),
+              init_layers('relu', {}),
               init_layers('flatten', {}),
-            #   init_layers('linear', {'num_in': 63,
-            #                          'num_out': 50}), 
-            #   init_layers('relu', {}),                
-            #   init_layers('linear', {'num_in': 50,
-            #                          'num_out': 40}), 
-            #   init_layers('relu', {}),  
-            #   init_layers('linear', {'num_in': 40,
-            #                          'num_out': 30}), 
-            #   init_layers('relu', {}),  
-            #   init_layers('linear', {'num_in': 63,
-            #                          'num_out': 40}),  
-              init_layers('relu', {}), 
-              init_layers('linear', {'num_in': 63,
-                                     'num_out': 6}),            
+              init_layers('linear', {'num_in': 105,
+                                     'num_out': 60}),      
+              init_layers('relu', {}),
+              init_layers('linear', {'num_in': 60,
+                                     'num_out': 35}),      
+              init_layers('relu', {}),
+              init_layers('linear', {'num_in': 35,
+                                     'num_out': 15}),      
+              init_layers('relu', {}),
+              init_layers('linear', {'num_in': 15,
+                                     'num_out': 6}),      
               init_layers('softmax', {})]
         
-    model = init_model(layers, [3, 21, 1], 6, True)
+    model = init_model(layers, [21, 3, 1], 6, True)
     params = {"test_data": test_data,
               "test_labels": test_label}
         
-    train_model, train_loss = train(model, train_data, train_label, params, 5000)
+    train_model, train_loss = train(model, train_data, train_label, params, 3000)
 
 if __name__ == '__main__':
     print('Training!')
